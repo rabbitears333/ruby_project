506503b92056b588dd3c4eb98be277bbb5e7e4bf
Andy Wang <rabbitears333@gmail.com>
2016-12-27 15:00:18 +0000

    signied in users

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -3,12 +3,11 @@ require "rails_helper"
 RSpec.feature "Listing Articles" do
     before do
         @john = User.create(email: "hogn@hotmail.com", password: "password")
-        @article1 = Article.create(title: "The first article", body: "test text now", user: john)
-        
-        @article2 = Article.create(title: "The second article", body: "test text now of second article", user: john)
+        @article1 = Article.create(title: "The first article", body: "test text now", user: @john)
+        @article2 = Article.create(title: "The second article", body: "test text now of second article", user: @john)
     
     end
-    scenario "A user lists all articles" do
+    scenario "with articles created and users not signed in" do
         visit "/"
         expect(page).to have_content(@article1.title)
         expect(page).to have_content(@article1.body)
@@ -17,6 +16,7 @@ RSpec.feature "Listing Articles" do
         expect(page).to have_content(@article2.body)
         expect(page).to have_link(@article1.title)
         expect(page).to have_link(@article2.title)
+        expect(page).not_to have_link("New Article")
     end
     scenario "A user lists no articles" do
         Article.delete_all
@@ -32,5 +32,29 @@ RSpec.feature "Listing Articles" do
         within("h1#no-articles") do
             expect(page).to have_content("No Articles Created")
         end
+    end
+    
+    scenario "A user lists all articles" do
+        visit "/"
+        expect(page).to have_content(@article1.title)
+        expect(page).to have_content(@article1.body)
+        
+        expect(page).to have_content(@article2.title)
+        expect(page).to have_content(@article2.body)
+        expect(page).to have_link(@article1.title)
+        expect(page).to have_link(@article2.title)
+    end
+    
+    scenario "with articles created and users signed in" do
+        login_as(@john)
+        visit "/"
+        expect(page).to have_content(@article1.title)
+        expect(page).to have_content(@article1.body)
+        
+        expect(page).to have_content(@article2.title)
+        expect(page).to have_content(@article2.body)
+        expect(page).to have_link(@article1.title)
+        expect(page).to have_link(@article2.title)
+        expect(page).to have_link("New Article")
     end
 end
