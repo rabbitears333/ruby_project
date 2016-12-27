0e737dacd1be9e61e2120c77eb6c2fa624b8c4c7
Andy Wang <rabbitears333@gmail.com>
2016-12-27 15:06:33 +0000

    updated users

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -3,16 +3,32 @@ require "rails_helper"
 RSpec.feature "Showing Articles" do
     before do
         @john = User.create(email: "hogn@hotmail.com", password: "password")
-        login_as(john)
+        @fred = User.create(email: "fred@example.com", password: "password")
         @article = Article.create(title: "The first article", body: "sample text", user: john)
     end
     
-    scenario " A user shows an article" do
+    scenario "To non-signed in user hide the Edit and Delete buttons" do
         visit "/"
         click_link @article.title
         
         expect(page).to have_content(@article.title)
         expect(page).to have_content(@article.body)
         expect(current_path).to eq(article_path(@article))
+        
+        expect(page).not_to have_link("Edit Article")
+        expect(page).not_to have_link("Delete Article")
+    end
+    
+    scenario "To non-owner hide the Edit and Delete buttons" do
+        login_as(@fred)
+        visit "/"
+        click_link @article.title
+        
+        expect(page).to have_content(@article.title)
+        expect(page).to have_content(@article.body)
+        expect(current_path).to eq(article_path(@article))
+        
+        expect(page).not_to have_link("Edit Article")
+        expect(page).not_to have_link("Delete Article")
     end
 end
